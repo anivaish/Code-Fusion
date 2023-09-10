169f03c2158f7505926dd8746357609c971d1cc5
Aniruddh Vaish <aniruddhvaish2024@gmail.com>
2023-09-10 09:29:07 +0530

    api updated

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -115,7 +115,7 @@ int main()
                     lang: option.value
                 }
                 console.log(JSON.stringify(code));
-                const outData = await fetch("http://localhost:8000/compile", {
+                const outData = await fetch("https://code-fusion-ani.onrender.com/compile", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
